7f215c0a05bc8c33d32f8ee688824515d1dd42d4
ungpyolee <uplee114@gmail.com>
2024-04-20 22:01:05 +0900

    JS 13 강아지 정보 페이지 프로젝트(1) - XMLHTTPRequest 생성자를 통해 강아지 이미지, 견종 받아오기 - 받아온 강아지 이미지 보여주기 - 받아온 견종의 키값을 Object.keys() 메소드를 통해 배열로 받기 - 배열로 받은 견종 option으로 넣기

---
+++ 02_Javascript/13_비동기데이터통신/breeds.js
@@ -0,0 +1,48 @@
+// https://dog.ceo/api/breeds/list/all < 모든 견종의 정보를 받는 주소
+// https://dog.ceo/api/breeds/image/random/42  < 42마리의 강아지 이미지를 받는 주소
+
+const apiRandomDogs = "https://dog.ceo/api/breeds/image/random/42";
+const apiAllBreeds = "https://dog.ceo/api/breeds/list/all";
+const request1 = new XMLHttpRequest();
+const request2 = new XMLHttpRequest();
+
+const header = document.getElementById("header")
+const main = document.getElementById("main")
+const input = document.getElementById("filter-text")
+const button = document.getElementById("filter-button")
+const select = document.getElementById("filter-select")
+
+const currentDogs = [];
+
+window.addEventListener("load", function(){
+
+    // 강아지 사진 뿌리기
+    request1.open("get", apiRandomDogs)
+    request1.addEventListener('load', function(){
+        const response = JSON.parse(request1.response);
+        response.message.forEach(function(item){
+            currentDogs.push(item);
+            const dogImgDiv = document.createElement('div');
+            dogImgDiv.classList.add("flex-item")
+            dogImgDiv.innerHTML = `
+                <img src=${item}>
+            `
+            main.appendChild(dogImgDiv);
+        })
+    })
+    request1.send();
+
+    // 셀렉트에 견종 정보 뿌리기
+    request2.open('get', apiAllBreeds);
+    request2.addEventListener("load", function(){
+        const response = JSON.parse(request2.response);
+        // key 값 받아오기
+        Object.keys(response.message).forEach(function(item){
+            const option = document.createElement("option")
+            option.textContent = item;
+            option.value = item;
+            select.appendChild(option);
+        })
+    })
+    request2.send();
+})
