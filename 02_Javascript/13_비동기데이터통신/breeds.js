02ac6242d7a08e77952341895ba3ab6f6347176f
ungpyolee <uplee114@gmail.com>
2024-04-20 22:42:54 +0900

    JS 13 강아지 정보 페이지 프로젝트(2)
    - 이미지 주소에 있는 견종과 input.value값과 일치한 이미지 반환하여 보여주기
    - select option.value값과 일치한 이미지 반환하여 보여주기
    - 연속적으로 사용되는 내용 함수로 만들어서 쓰기

--- 02_Javascript/13_비동기데이터통신/breeds.js
+++ 02_Javascript/13_비동기데이터통신/breeds.js
@@ -14,30 +14,34 @@ const select = document.getElementById("filter-select")
 
 const currentDogs = [];
 
-window.addEventListener("load", function(){
+function displayDogs(item) {
+    const dogImgDiv = document.createElement('div');
+    dogImgDiv.classList.add("flex-item")
+    dogImgDiv.innerHTML = `
+                <img src=${item}>
+            `
+    main.appendChild(dogImgDiv);
+}
+
+window.addEventListener("load", function () {
 
     // 강아지 사진 뿌리기
     request1.open("get", apiRandomDogs)
-    request1.addEventListener('load', function(){
+    request1.addEventListener('load', function () {
         const response = JSON.parse(request1.response);
-        response.message.forEach(function(item){
+        response.message.forEach(function (item) {
             currentDogs.push(item);
-            const dogImgDiv = document.createElement('div');
-            dogImgDiv.classList.add("flex-item")
-            dogImgDiv.innerHTML = `
-                <img src=${item}>
-            `
-            main.appendChild(dogImgDiv);
+            displayDogs(item)
         })
     })
     request1.send();
 
     // 셀렉트에 견종 정보 뿌리기
     request2.open('get', apiAllBreeds);
-    request2.addEventListener("load", function(){
+    request2.addEventListener("load", function () {
         const response = JSON.parse(request2.response);
         // key 값 받아오기
-        Object.keys(response.message).forEach(function(item){
+        Object.keys(response.message).forEach(function (item) {
             const option = document.createElement("option")
             option.textContent = item;
             option.value = item;
@@ -45,4 +49,26 @@ window.addEventListener("load", function(){
         })
     })
     request2.send();
+})
+
+button.addEventListener('click', function () {
+    main.innerHTML = "";
+    let filteredDogs = currentDogs.filter(function (item) {
+        return item.indexOf(input.value) !== -1
+    })
+
+    input.value = "";
+    filteredDogs.forEach(function (item) {
+        displayDogs(item)
+    })
+})
+
+select.addEventListener('change', function () {
+    main.innerHTML = "";
+    let filteredDogs = currentDogs.filter(function (item) {
+        return item.indexOf(select.value) !== -1
+    })
+    filteredDogs.forEach(function (item) {
+        displayDogs(item)
+    })
 })
