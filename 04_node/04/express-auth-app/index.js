b142a70a5cc3236231b13dd93efcf8824a4e91db
ungpyolee <uplee114@gmail.com>
2024-05-02 15:52:31 +0900

    node Practice 04 인증시스템 구현하기 1. Client > 유저 인증 요청하기 2. Server > 유저 확인 후 accessToken, refreshToken 발급하기 3. Client > accessToken으로 인증 요청하기 4. Server > accessToken 유효한지 확인 후 Protected resource 보내주기 5. Client > 유효시간이 만료된 상황에서 accessToken으로 인증 요청하기 6. Server > 만료된 accessToken에 대하여 유효하지않은 token error 보내주기 7. Client > refreshToken으로 다시 요청하기 8. Server > refreshToken이 유효하면 accessToken 새로 발급해주기

---
+++ 04_node/04/express-auth-app/index.js
@@ -0,0 +1,99 @@
+const cookieParser = require('cookie-parser')
+const express = require('express');
+const jwt = require('jsonwebtoken')
+
+const app = express();
+const secretText = 'superSecret';
+const refreshSecretText = 'supersuperSecret'
+
+const posts = [
+    {
+        username : 'John',
+        title : 'Post 1'
+    },
+    {
+        username : 'Han',
+        title : 'Post 2'
+    }
+]
+let refreshTokens = []
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = { name : username };
+
+    // jwt를 이용해서 토큰 생성하기 payload + secretText
+    // 유효기간 추가
+    const accessToken = jwt.sign(user,
+        secretText,
+        {expiresIn :'30s'});
+
+    // jwt를 이용해서 refresthToken도 생성
+    const refreshToken = jwt.sign(user,
+        refreshSecretText,
+        {expiresIn : '1d'});
+        refreshTokens.push(refreshToken);
+
+    // refreshToken을 cookie에 넣어주기
+    res.cookie('jwt', refreshToken,{
+        httpOnly : true,
+        maxAge : 24 * 60 * 60 * 1000
+    })
+
+    res.json({accessToken : accessToken});
+
+
+})
+
+app.get('/posts', authMiddleware, (req, res) => {
+    res.json(posts)
+})
+
+function authMiddleware(req, res, next){
+    // 토큰을 request headers에서 가져오기
+    const authHeader = req.headers['authorization'];
+    // Bearer kjfoessdfjksld.fdjsidjfkls.sdfjksdlfjkls
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    // 토큰이 있으니 유효한 토큰인지 확인
+    jwt.verify(token, secretText, (err, user) => {
+        if(err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    })
+}
+
+app.get('/refresh', (req, res) =>{
+
+    // body =>  parsing =>  req.body
+    // cookies  =>  parsing =>  req.cookies
+
+    // cookies 가져오기 cookie-parser
+    const cookies = req.cookies;
+    if(!cookies?.jwt) return res.sendStatus(403);
+    const refreshToken = cookies.jwt;
+    // refreshToken이 데이터베이스에 있는 토큰인지 확인
+    if(!refreshToken.includes(refreshToken)){
+        return res.sendStatus(403)
+    }
+
+    // token 이 유효한 토큰인지 확인
+    jwt.verify(refreshToken, refreshSecretText, (err, user) => {
+        if(err) return res.sendStatus(403);
+        // accessToken을 생성하기
+        const accessToken = jwt.sign({ name : user.name },
+            secretText,
+            {expiresIn : '30s'}
+        )
+        res.json({ accessToken })
+    })
+})
+
+const port = 4000;
+app.listen(port, () => {
+    console.log('listening on port ' + port)
+})
