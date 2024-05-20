29db084e3b90d5ce1b690c81a78eacdd16144dee
ungpyolee <uplee114@gmail.com>
2024-05-20 23:58:29 +0900

    api instance 생성 및 요청보내기

---
+++ 05_react_redux/03/react-apple-app/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL : "https://api.themoviedb.org/3",
+    prams: {
+        api_key: "8aaa365c26fff949ebec37c6c3680a3f",
+        language: "ko-KR"
+    }
+});
+
+export default instance;
