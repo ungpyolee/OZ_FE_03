cebcb8434b07d161d34557c7bd1c6f0b8fcc81ca
ungpyolee <uplee114@gmail.com>
2024-05-21 20:44:07 +0900

    구조 파일 생성

--- 05_react_redux/02/react-lazy/src/App.js
+++ 05_react_redux/02/react-lazy/src/App.js
@@ -1,6 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes, Link, Outlet} from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+// import Blog from './components/Blog';
+import { Suspense, lazy } from 'react';
+
+const Blog = lazy(() => import('./components/Blog'));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+
+          <Route index element={<Home />}/>
+          <Route path='about' element={<About />}/>
+          <Route path='blog' element={<Blog />}/>
+
+        </Route>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 function Layout() {
   return (
@@ -8,25 +31,20 @@ function Layout() {
     <ul>
       <li>
         <Link to={'/'}>Home</Link>
+      </li>
+      <li>
         <Link to={'/about'}>About</Link>
+      </li>
+      <li>
         <Link to={'/blog'}>Blog</Link>
       </li>
     </ul>
+    <Suspense fallback={<h1>Loading...</h1>}>
       <Outlet />
+    </Suspense>
     </>
   )
 }
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-        </Route>
-        <Route />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
 export default App;
---
+++ 05_react_redux/02/react-lazy/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+        
+        <button onClick={() => {
+            import('../utils/index').then(module => {
+                alert(module.sum(1,2))
+            })
+            // alert(sum(1, 1));
+        }}>
+            +
+        </button>
+    </div>
+  )
+}
+
+export default Home
