b075ab9af7bf1335ff68b776e3bf4e06204c3f99
ungpyolee <uplee114@gmail.com>
2024-05-22 21:29:30 +0900

    네비게이션 컴포넌트 생성 및 스타일링

--- 05_react_redux/03/react-apple-app/src/components/Nav.jsx
+++ 05_react_redux/03/react-apple-app/src/components/Nav.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { styled } from 'styled-components' 
+
 
 const Nav = () => {
   return (
@@ -6,12 +8,40 @@ const Nav = () => {
       <Logo>
         <img
           alt="logo"
-          src="/image/apple-logo.png"
+          src="/public/images/apple-logo.png"
           onClick={()=> (window.location.herf = '/')}
           />
       </Logo>
     </NavWrapper>
   )
 }
+
+const NavWrapper = styled.nav`
+  position : fixed;
+  top : 0;
+  left: 0;
+  right : 0;
+  height : 70px;
+  background-color : #000000;
+  display : flex;
+  justify-content : space-between;
+  align-items : center;
+  padding : 0 36px;
+  letter-spacing : 16px;
+  z-index : 3;
+`
+
+const Logo = styled.a`
+  padding : 0;
+  width : 70px;
+  font-size : 0;
+  display : inline-block;
+  margin-bottom : 10px;
+
+  img {
+    display : block;
+    width : 100%;
+  }
+`
 
 export default Nav
