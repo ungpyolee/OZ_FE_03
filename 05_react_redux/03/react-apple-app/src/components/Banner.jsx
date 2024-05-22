0c70887b41f85e76ea22f9a0a1b270c50e526df5
ungpyolee <uplee114@gmail.com>
2024-05-23 00:18:49 +0900

    배너 랜덤으로 받아오기 1. 영화정보 랜덤으로 받아서 비디오 있는지 없는지 확인하고 버튼 보여주기 2. 영화 제목, 줄거리, 이미지 받아와서 보여주기 3. css 스타일 적용하기

--- 05_react_redux/03/react-apple-app/src/components/Banner.jsx
+++ 05_react_redux/03/react-apple-app/src/components/Banner.jsx
@@ -26,9 +26,48 @@ const Banner = () => {
       setMovie(movieDetail);
   }
 
-  return (
-    <div>Banner</div>
-  )
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  }
+
+  if(!movie){
+    return (
+      <div>Loading ... </div>
+    )
+  } else {
+    return (
+      <div
+        className='banner'
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition : 'top center',
+          backgroundSize : 'cover',
+        }}
+      >
+        <div className='banner__contents'>
+          <h1 className='banner__title'>
+            {movie.title || movie.name || movie.original_name}
+          </h1>      
+          <div className='banner__buttons'>
+            {movie.videos?.results[0]?.key ?
+            <button
+              className='banner__button play'
+            >
+              Play
+            </button>  
+            : null
+          }
+          </div>  
+          <p className='banner_description'>
+            {truncate(movie.overview, 100)}
+          </p>
+        </div>
+        <div className='banner--fadeBottom' />  
+      </div>
+    )
+  }
+
+ 
 }
 
 export default Banner
