838d48cb52febca2f9139b7f510fbcb7e768c130
ungpyolee <uplee114@gmail.com>
2024-05-23 14:35:29 +0900

    Row 컴포넌트 생성 및 스타일작업

--- 05_react_redux/03/react-apple-app/src/App.jsx
+++ 05_react_redux/03/react-apple-app/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 import Nav from './components/Nav'
 import Banner from './components/Banner'
+import Row from './components/Row'
 import { styled } from "styled-components"
+import requests from './api/requests';
 
 function App() {
 
@@ -9,6 +11,10 @@ function App() {
     <Container>
       <Nav />
       <Banner />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </Container>
   )
 }
