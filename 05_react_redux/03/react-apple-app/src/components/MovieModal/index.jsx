e087a2b5192b91d34a34dfa3d693c4da9d34c7c4
ungpyolee <uplee114@gmail.com>
2024-05-27 13:31:01 +0900

    Movie 모달 UI 생성 및 css 적용

--- 05_react_redux/03/react-apple-app/src/components/MovieModal/index.jsx
+++ 05_react_redux/03/react-apple-app/src/components/MovieModal/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { imageBasePath } from '../constant'
+import './MovieModal.css'
 
 const MovieModal = ({
-    backgrop_path,
+    backdrop_path,
     title,
     overview,
     name,
@@ -11,7 +13,38 @@ const MovieModal = ({
     setModalOpen
 }) => {
   return (
-    <div>MovieModal</div>
+    <div className='presentation' role='presentation'>
+      <div className='wrapper-modal'>
+        <div className='modal'>
+          <span 
+            onClick={() => setModalOpen(false)}
+            className='modal-close'
+          >
+            X
+          </span>
+
+          <img
+            className='modal__poster-img'
+            src={`${imageBasePath}${backdrop_path}`}
+            alt='modal_poster-img'
+            />
+
+          <div className='modal__content'>
+            <p className='modal__details'>
+              <span>
+                100% for you
+              </span>{" "}
+              {release_date ? release_date : first_air_date}
+            </p>
+            <h2 className='modal__title'>
+              {title ? title : name}
+            </h2>
+            <p className='modal__overview'>퍙점 : {vote_average}</p>
+            <p className='modal__overview'>{overview}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
