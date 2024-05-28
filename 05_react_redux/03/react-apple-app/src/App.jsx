5a8956f4af33d069c6ee6e36aab5e0c0cb31e824
ungpyolee <uplee114@gmail.com>
2024-05-28 23:40:12 +0900

    page생성 및 Route 연결

--- 05_react_redux/03/react-apple-app/src/App.jsx
+++ 05_react_redux/03/react-apple-app/src/App.jsx
@@ -1,29 +1,38 @@
+
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Nav from './components/Nav'
-import Banner from './components/Banner'
-import Row from './components/Row'
-import { styled } from "styled-components"
-import requests from './api/requests';
+import LoginPage from './pages/LoginPage'
+import MainPage from './pages/MainPage'
+import DetailPage from './pages/DetailPage'
+import SearchPage from './pages/SearchPage'
+
+const Layout = () => {
+  return (
+    <>
+      <Nav />
+
+      <Outlet />
+    </>
+  )
+}
 
 function App() {
 
   return (
-    <Container>
-      <Nav />
-      <Banner />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
-    </Container>
+    <>
+      <Routes>   
+        <Route path='/' element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path='main' element={<MainPage />} />
+          <Route path=':movieId' element={<DetailPage />} />
+          <Route path='search' element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
-const Container = styled.main`
-  position : relative;
-  display : block;
-  top : 72px;
-  padding : 0 calc(3.5vw + 5px);
-`
+
 
 export default App
