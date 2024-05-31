e6c35d9fc15d5843db6918f3a4ff7f971a78edc2
ungpyolee <uplee114@gmail.com>
2024-05-31 23:59:54 +0900

    지출관리 Context 추가

--- 05_react_redux/06/react-budget-app/src/index.js
+++ 05_react_redux/06/react-budget-app/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppContextProvider } from './contexts/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AppContextProvider>
     <App />
-  </React.StrictMode>
+  </AppContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
