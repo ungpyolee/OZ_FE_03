e597a7b154f234038105997ee418fcb553c14ce6
ungpyolee <uplee114@gmail.com>
2024-06-09 16:33:56 +0900

    create tdd-counter-app, .prettierrc.js, .eslintrc.json set, 테스트 작성(Red 단계)

---
+++ 05_react_redux/09/tdd-counter-app/src/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('초기 카운트는 0이다', () => {
+  render(<App />)
+  expect(screen.getByText(/카운트: 0/i)).toBeInTheDocument()
+})
+
+test('"증가" 버튼을 클릭하면 카운트가 1 증가한다.', () => {
+  render(<App />)
+  fireEvent.click(screen.getByText(/증가/i))
+  expect(screen.getByText(/카운트: 1/i)).toBeInTheDocument()
+})
+
+test('"감소" 버튼을 클릭하면 카운트가 1 감소한다', () => {
+  render(<App />)
+  fireEvent.click(screen.getByText(/감소/i))
+  expect(screen.getByText(/카운트: -1/i)).toBeInTheDocument()
+})
