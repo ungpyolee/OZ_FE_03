3129aa985820d3440db0d4f70e2df8597041d1d3
ungpyolee <uplee114@gmail.com>
2024-06-09 16:38:58 +0900

    red단계 테스트를 위한 최소한의 tag를 App.js에 작성

--- 05_react_redux/09/tdd-counter-app/src/App.js
+++ 05_react_redux/09/tdd-counter-app/src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react'
 
 function App() {
   return (
     <div>
-      
+      <h1>카운트:</h1>
+      <button>증가</button>
+      <button>감소</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
