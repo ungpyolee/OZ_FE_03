da87379c373818a452126da05670821de947d011
ungpyolee <uplee114@gmail.com>
2024-07-04 00:31:58 +0900

    file name update

--- 05_react_redux/03/react-apple-app/src/pages/SearchPage/index.jsx
+++ 05_react_redux/03/react-apple-app/src/pages/SearchPage/index.jsx
@@ -1,77 +1,64 @@
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom"
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from '../../api/axios';
-import "./SearchPage.css";
-import { useDebounce } from "../../hooks/useDebounce";
+import './SearchPage.css';
+import { useDebounce } from '../../hooks/useDebounce';
 
 const SearchPage = () => {
+    const [searchResults, setSearchResults] = useState([]);
 
-  const [searchResults, setSearchResults] = useState([]);
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    const useQuery = () => {
+        return new URLSearchParams(useLocation().search);
+    };
 
-  const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
-  }
+    let query = useQuery();
+    const searchTerm = query.get('q');
+    const debouncedSearchTerm = useDebounce(query.get('q'), 500);
 
-  let query = useQuery();
-  const searchTerm = query.get('q');
-  const debouncedSearchTerm = useDebounce(query.get('q'), 500);
+    useEffect(() => {
+        if (debouncedSearchTerm) {
+            fetchSearchMovie(debouncedSearchTerm);
+        }
+    }, [debouncedSearchTerm]);
 
-  useEffect(() => {
-    if (debouncedSearchTerm) {
-      fetchSearchMovie(debouncedSearchTerm);
-    }
-  }, [debouncedSearchTerm])
+    const fetchSearchMovie = async (searchTerm) => {
+        try {
+            const response = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+            console.log(response);
+            setSearchResults(response.data.results);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  const fetchSearchMovie = async (searchTerm) => {
-    try {
-      const response = await axios.get(
-        `/search/multi?include_adult=false&query=${searchTerm}`
-      )
-      console.log(response);
-      setSearchResults(response.data.results);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  if (searchResults.length > 0) {
-    return (
-      <section className="search-container">
-        {searchResults.map((movie) => {
-          if (movie.backdrop_path !== null && movie.media_type !== "person") {
-            const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
-            return (
-              <div className="movie" key={movie.id}>
-                <div
-                  onClick={() => navigate(`/${movie.id}`)}
-                  className="movie__column-poster"
-                >
-                  <img
-                    src={movieImageUrl}
-                    alt="movie"
-                    className="movie__poster"
-                  />
+    if (searchResults.length > 0) {
+        return (
+            <section className="search-container">
+                {searchResults.map((movie) => {
+                    if (movie.backdrop_path !== null && movie.media_type !== 'person') {
+                        const movieImageUrl = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
+                        return (
+                            <div className="movie" key={movie.id}>
+                                <div onClick={() => navigate(`/${movie.id}`)} className="movie__column-poster">
+                                    <img src={movieImageUrl} alt="movie" className="movie__poster" />
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
+            </section>
+        );
+    } else {
+        return (
+            <section className="no-results">
+                <div className="no-results__text">
+                    <p>찾고자하는 검색어 {searchTerm} 에 맞는 영화가 없습니다.</p>
                 </div>
-              </div>
-            )
-          }
-        })}
-      </section>
-    )
-  } else {
-    return (
-      <section className="no-results">
-        <div className="no-results__text">
-          <p>
-            찾고자하는 검색어 {searchTerm} 에 맞는 영화가 없습니다.
-          </p>
-        </div>
-      </section>
-    )
-  }
+            </section>
+        );
+    }
+};
 
-}
-
-export default SearchPage
+export default SearchPage;
